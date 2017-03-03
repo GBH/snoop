e20036368ad4bf9a44d82fef8cc07c45b663f604
Oleg <oleg@khabarov.ca>
2017-03-02 16:55:09 -0800

    showing links now, adding license

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Snoop
 
-Firefox extension that automatically snoops on Reddit commenter's post history.
+Firefox WebExtension that automatically snoops on Reddit commenter's post history.
+
+Get it here: https://addons.mozilla.org/en-us/firefox/addon/redditsnoop/
--- snoop.js
+++ snoop.js
@@ -1,6 +1,13 @@
+// setting up event listener on all redditor username links
+document.body.addEventListener("mouseover", function(e){
+  if(e.target && e.target.matches("a.author")){
+    snoop(e.target);
+  }
+})
+
 function snoop(author) {
 
-  // already snopped, no reason to do it again
+  // already snooped, no reason to do it again
   if(author.parentNode.querySelector("span.snoop")){
     return false;
   }
@@ -8,43 +15,66 @@ function snoop(author) {
   // Creating span for output
   var snoop_span = document.createElement("span")
   snoop_span.className = "snoop";
-  snoop_span.innerHTML = " [snooping...] "
+  snoop_span.appendChild(document.createTextNode(" [snooping...] "));
   author.parentNode.insertBefore(snoop_span, null);
 
   var xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function(){
-    if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200){
-      let html = document.implementation.createHTMLDocument().documentElement;
-      html.innerHTML = xhr.responseText;
-      let subreddits_elements = html.querySelectorAll("a.subreddit");
-
-      let subreddits = {};
-      for (i = 0; i < subreddits_elements.length; i++){
-        let name = subreddits_elements[i].innerHTML.replace("r/", "");
-        subreddits[name] = subreddits[name] ? subreddits[name] + 1 : 1;
-      }
-
-      let sortable = []
-      for (let r in subreddits){
-        sortable.push([r, subreddits[r]])
-      }
-      sortable = sortable.sort(function(a,b){
-        return b[1] - a[1]
-      })
-      sortable = sortable.map(function(x){
-        return x[0]
-      });
-      sortable = sortable.slice(0, 5)
-
-      snoop_span.innerHTML = " [posts in: " + sortable.join(", ") + "] ";
-    }
-  }
+  xhr.onreadystatechange = function(){populate_snooped_history(xhr, snoop_span)};
   xhr.open('GET', author.getAttribute("href"), true);
   xhr.send();
 }
 
-document.body.addEventListener("mouseover", function(e){
-  if(e.target && e.target.matches("a.author")){
-    snoop(e.target);
+function populate_snooped_history(xhr, snoop_span){
+  if(xhr.readyState !== XMLHttpRequest.DONE || xhr.status !== 200){
+    return false;
   }
-})
+
+  let html = document.implementation.createHTMLDocument().documentElement;
+  html.innerHTML = xhr.responseText;
+  let subreddits_elements = html.querySelectorAll("a.subreddit");
+
+  let sub_names = {};
+  let sub_urls = {};
+  for (var i = 0; i < subreddits_elements.length; i++){
+    let name = subreddits_elements[i].innerHTML.replace("r/", "");
+    let url  = subreddits_elements[i].getAttribute("href");
+
+    sub_names[name] = sub_names[name] ? sub_names[name] + 1 : 1;
+    sub_urls[name] = url;
+  }
+
+  // Sorting by most frequent subreddits
+  let sorted_names = []
+  for (let r in sub_names){
+    sorted_names.push([r, sub_names[r]])
+  }
+  sorted_names = sorted_names.sort(function(a,b){
+    return b[1] - a[1]
+  })
+  sorted_names = sorted_names.map(function(x){
+    return x[0]
+  });
+  sorted_names = sorted_names.slice(0, 5)
+
+  // blanking and populating snoop span
+  while (snoop_span.firstChild) {
+    snoop_span.removeChild(snoop_span.firstChild);
+  }
+
+  snoop_span.appendChild(document.createTextNode(" [posts in: "));
+  for (var i = 0; i < sorted_names.length; i ++){
+    let name = sorted_names[i];
+
+    let link = document.createElement("a");
+    link.href = sub_urls[name];
+    link.target = "_blank";
+    link.appendChild(document.createTextNode(name));
+
+    snoop_span.appendChild(link);
+
+    if (i != sorted_names.length - 1){
+      snoop_span.appendChild(document.createTextNode(", "));
+    }
+  }
+  snoop_span.appendChild(document.createTextNode("] "));
+}
